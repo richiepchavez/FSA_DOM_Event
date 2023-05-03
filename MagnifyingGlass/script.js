8a13eeda28653795335400135316f564c7c5b55a
Leo Harada <leotharada@gmail.com>
2023-05-03 15:09:40 -0700

    Adding GreenScreen, Magnifying Glass and CraneGame excercises. Was not able to complete the CraneGame... unfortunately...

---
+++ MagnifyingGlass/script.js
@@ -0,0 +1,12 @@
+const makeLarge = document.querySelector('.largeButton')
+const makeSmall = document.querySelector('.smallButton')
+const largeText = document.querySelector('.large')
+const smallText = document.querySelector('.small')
+
+function largeFunction() {
+    largeText.style.fontSize = "50px";
+}
+
+function smallFunction() {
+    smallText.style.fontSize = "10px"
+}
